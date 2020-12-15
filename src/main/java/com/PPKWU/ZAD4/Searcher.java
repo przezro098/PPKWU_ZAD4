64b8b43b1718790fb689052b395b41611715cc09
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-12-15 15:18:25 +0100

    Searcher class init

--- src/main/java/com/PPKWU/ZAD4/Searcher.java
+++ src/main/java/com/PPKWU/ZAD4/Searcher.java
@@ -1,5 +1,18 @@
 package com.PPKWU.ZAD4;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class Searcher {
 
+    @RequestMapping(value="/field", method = RequestMethod.GET)
+    public List<Result> getResults(String searchedField) {
+
+        List<Result> results = new ArrayList<>();
+    }
 }
