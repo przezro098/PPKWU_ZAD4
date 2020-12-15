acf7ffd961bfe46656fee045c71560bf15448f52
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-12-15 15:48:39 +0100

    Started data processing...

--- src/main/java/com/PPKWU/ZAD4/Searcher.java
+++ src/main/java/com/PPKWU/ZAD4/Searcher.java
@@ -2,6 +2,8 @@ package com.PPKWU.ZAD4;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +22,18 @@ public class Searcher {
 
         List<Result> results = new ArrayList<>();
         Document document = Jsoup.connect(url+searchedField+"&l=").get();
-        
+        Elements elements = document.select("script");
+        for(Element element : elements)
+        {
+            if(element.attr("type").equals("application/ld+json"))
+            {
+                Result result = new Result();
+                result.setName(element.dataset().get("name"));
+                result.setEmail(element.dataset().get("email"));
+                result.setAddress(element.dataset().get("name"));
+                result.setName(element.dataset().get("name"));
+            }
+        }
 
 
     }
