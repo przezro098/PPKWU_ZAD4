367da8d8e4aeb134c6fb7239ef7320cef84420f1
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-12-15 15:32:13 +0100

    Jsoup connection added

--- src/main/java/com/PPKWU/ZAD4/Searcher.java
+++ src/main/java/com/PPKWU/ZAD4/Searcher.java
@@ -1,9 +1,12 @@
 package com.PPKWU.ZAD4;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +16,11 @@ public class Searcher {
     private String url = "https://panoramafirm.pl/szukaj?k=";
 
     @RequestMapping(value="/field", method = RequestMethod.GET)
-    public List<Result> getResults(String searchedField) {
+    public List<Result> getResults(String searchedField) throws IOException {
 
         List<Result> results = new ArrayList<>();
-
+        Document document = Jsoup.connect(url+searchedField+"&l=").get();
+        
 
 
     }
